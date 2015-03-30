22c566b908671496ceb079ab26ebf57aafe9d96d
abcmumubin <2631593210@qq.com>
2015-03-30 16:32:51 +0800

    the code of lab1
    实验1的项目代码，只是运行结果为乱码

---
+++ lab1_324_libin/lab1_324.sdk/SDK/SDK_Export/324_test/Debug/src/memorytest.d
@@ -0,0 +1,29 @@
+src/memorytest.d: ../src/memorytest.c \
+ ../../324_test_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../324_test_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../324_test_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../../324_test_bsp/ps7_cortexa9_0/include/xstatus.h \
+ ../../324_test_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../../324_test_bsp/ps7_cortexa9_0/include/xil_assert.h \
+ ../../324_test_bsp/ps7_cortexa9_0/include/xil_testmem.h \
+ ../src/platform.h ../src/platform_config.h ../src/memory_config.h
+
+../../324_test_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../324_test_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../324_test_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../../324_test_bsp/ps7_cortexa9_0/include/xstatus.h:
+
+../../324_test_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../../324_test_bsp/ps7_cortexa9_0/include/xil_assert.h:
+
+../../324_test_bsp/ps7_cortexa9_0/include/xil_testmem.h:
+
+../src/platform.h:
+
+../src/platform_config.h:
+
+../src/memory_config.h:
---
+++ lab1_324_libin/lab1_324.sdk/SDK/SDK_Export/324_test/Debug/src/platform.d
@@ -0,0 +1,16 @@
+src/platform.d: ../src/platform.c \
+ ../../324_test_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../324_test_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../324_test_bsp/ps7_cortexa9_0/include/xil_cache.h \
+ ../../324_test_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../src/platform_config.h
+
+../../324_test_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../324_test_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../324_test_bsp/ps7_cortexa9_0/include/xil_cache.h:
+
+../../324_test_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../src/platform_config.h:
